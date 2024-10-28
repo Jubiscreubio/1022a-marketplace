15c208efa2d9c06e529bc94056d5ed252d9d567a
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2024-10-28 10:21:04 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,22 @@ app.get("/produtos", async (req, res) => {
     }
 })
 
+app.get("/produtos", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022a",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const [result, fields] = await connection.query("INSERT INTO produtos VALUES (?,?,?,?,?)")
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        res.status(500).send("Server ERROR")
+    }
+})
 
 app.get("/usuarios", async (req, res) => {
     try {
