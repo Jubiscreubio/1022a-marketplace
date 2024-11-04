de59fb8162c8322a2925e50ce98f12b5edd2bac2
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2024-11-04 08:42:17 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,7 @@ app.get("/produtos", async (req, res) => {
         res.status(500).send("Server ERROR")
     }
 })
-
-app.get("/produtos", async (req, res) => {
+app.post("/produtos", async (req, res) => {
     try {
         const connection = await mysql.createConnection({
             host: process.env.dbhost ? process.env.dbhost : "localhost",
@@ -33,13 +32,18 @@ app.get("/produtos", async (req, res) => {
             database: process.env.dbname ? process.env.dbname : "banco1022a",
             port: process.env.dbport ? parseInt(process.env.dbport) : 3306
         })
-        const [result, fields] = await connection.query("INSERT INTO produtos VALUES (?,?,?,?,?)")
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result, fields] = 
+                    await connection.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+                            [id,nome,descricao,preco,imagem])
         await connection.end()
         res.send(result)
     } catch (e) {
+        console.log(e)
         res.status(500).send("Server ERROR")
     }
 })
+
 
 app.get("/usuarios", async (req, res) => {
     try {
