21b99553a4c9ce4347830b81420df829652f2ff7
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2024-12-04 11:46:37 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,23 @@ app.get("/usuarios", async (req, res) => {
     }
 })
 
+app.get("/carrinho", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022a",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const [result, fields] = await connection.query("SELECT * from carrinho")
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        res.status(500).send("Server ERROR")
+    }
+})
+
 app.listen(8000, () => {
     console.log("Iniciei o servidor")
 })
