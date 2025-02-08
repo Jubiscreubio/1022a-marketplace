bc2954ca37cde4be72644f3c152b63df6a84dc43
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2025-02-08 13:32:01 -0400

    test

--- src/db/banco-mongo.ts
+++ src/db/banco-mongo.ts
@@ -1,48 +1,56 @@
-import {MongoClient} from 'mongodb'
-class BancoMongo{
-    connection:MongoClient|null = null
-    async criarConexao(){
+import { MongoClient } from 'mongodb';
+
+class BancoMongo {
+    connection: MongoClient | null = null;
+
+    async criarConexao() {
         const url = 'mongodb://localhost:27017';
         const client = new MongoClient(url);
         this.connection = await client.connect();
     }
-    async finalizarConexao(){
-        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
-        await this.connection.close()
+
+    async finalizarConexao() {
+        if (!this.connection) throw new Error("Erro de conexão com o banco de dados.");
+        await this.connection.close();
     }
-    async listar(){
-        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+
+    async listar() {
+        if (!this.connection) throw new Error("Erro de conexão com o banco de dados.");
         const db = this.connection.db("defaultdb");
-        const result = db.collection('produtos').find().toArray()
-        return result
+        const result = db.collection('produtos').find().toArray();
+        return result;
     }
-    async inserir(produto:{id:number,titulo:string,detalhes:string,valor:string,foto:string,categoria:string,estoque:string}){
-        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+
+    async inserir(produto: { id: number, nome: string, descricao: string, preco: string, imagem: string }) {
+        if (!this.connection) throw new Error("Erro de conexão com o banco de dados.");
         const db = this.connection.db("defaultdb");
-        const result = db.collection('produtos').insertOne(produto)
-        return result
+        const result = db.collection('produtos').insertOne(produto);
+        return result;
     }
-    async excluir(id:string){
-        const idNumber = parseInt(id)
-        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+
+    async excluir(id: string) {
+        const idNumber = parseInt(id);
+        if (!this.connection) throw new Error("Erro de conexão com o banco de dados.");
         const db = this.connection.db("defaultdb");
-        const result = db.collection('produtos').deleteOne({id:idNumber})
-        return result
+        const result = db.collection('produtos').deleteOne({ id: idNumber });
+        return result;
     }
-    async alterar(id:string,produto:{id?:string,titulo:string,detalhes:string,valor:string,foto:string,categoria:string,estoque:string}){
-        const idNumber = parseInt(id)
-        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+
+    async alterar(id: string, produto: { nome: string, descricao: string, preco: string, imagem: string }) {
+        const idNumber = parseInt(id);
+        if (!this.connection) throw new Error("Erro de conexão com o banco de dados.");
         const db = this.connection.db("defaultdb");
-        const result = db.collection('produtos').updateOne({id:idNumber},{$set:produto})
-        return result
+        const result = db.collection('produtos').updateOne({ id: idNumber }, { $set: produto });
+        return result;
     }
-    async listarPorId(id:string){
-        const idNumber = parseInt(id)
-        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+
+    async listarPorId(id: string) {
+        const idNumber = parseInt(id);
+        if (!this.connection) throw new Error("Erro de conexão com o banco de dados.");
         const db = this.connection.db("defaultdb");
-        const result = db.collection('produtos').findOne({id:idNumber})
-        return result
+        const result = db.collection('produtos').findOne({ id: idNumber });
+        return result;
     }
 }
 
-export default BancoMongo
+export default BancoMongo;
