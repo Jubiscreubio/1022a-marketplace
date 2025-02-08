1ba82bd8131322851f9af991d5d553327885af1d
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2025-02-08 15:18:49 -0400

    test

--- src/db/banco-mysql.ts
+++ src/db/banco-mysql.ts
@@ -4,11 +4,11 @@ class BancoMysql {
     private conexao: Promise<Connection>;
     constructor() {
         this.conexao = mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "mysql-marketplace-estudante-57dd.f.aivencloud.com",
-            user: process.env.dbuser ? process.env.dbuser : "avnadmin",
-            password: process.env.dbpassword ? process.env.dbpassword : "AVNS_0a_3E2vf_5P_Q1FJapL",
-            database: process.env.dbname ? process.env.dbname : "defaultdb",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 13293
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022a",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 28355
         });
     }
 
@@ -45,32 +45,6 @@ class BancoMysql {
         const conn = await this.getConnection()
         const sqlQuery = "UPDATE produtos SET nome=?,descricao=?,imagem=? WHERE id = ?"
         const parametro = [produto.nome,produto.descricao,produto.imagem,id]
-        const [result, fields] = await conn.query(sqlQuery,parametro);
-        return result
-    }
-    async listarUser(){
-        const conn = await this.getConnection()
-        const [result, fields] = await conn.query("SELECT * from usuarios");
-        return result
-    }
-    async inserirUser(usuario:{id:number,nome:string,funcao:string,email:string,foto:string}){
-        const conn = await this.getConnection()
-        const sqlQuery = "INSERT INTO usuarios (id,nome,funcao,email,foto) VALUES (?,?,?,?,?)"
-        const parametro = [usuario.id,usuario.nome,usuario.funcao,usuario.email,usuario.foto]
-        const [result, fields] = await conn.query(sqlQuery,parametro);
-        return result
-    }
-    async excluirUser(id:string){
-        const conn = await this.getConnection()
-        const sqlQuery = "DELETE FROM usuarios WHERE id = ?"
-        const parametro = [id]
-        const [result, fields] = await conn.query(sqlQuery,parametro);
-        return result
-    }
-    async alterarUser(id:string,usuario:{id?:string,nome:string,funcao:string,email:string,foto:string}){
-        const conn = await this.getConnection()
-        const sqlQuery = "UPDATE usuarios SET nome=?,funcao=?,email=?,foto=? WHERE id = ?"
-        const parametro = [usuario.nome,usuario.funcao,usuario.email,usuario.foto,id]
         const [result, fields] = await conn.query(sqlQuery,parametro);
         return result
     }
